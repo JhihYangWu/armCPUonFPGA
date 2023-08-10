3ab25d86dc5f5762e722abff3edd3da6bb84c270
JhihYang Wu <jhihyang_wu@outlook.com>
2023-08-10 09:38:15 -0700

    Add top_tb.v.

---
+++ single_cycle/top_tb.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module top_tb();
+
+    reg Clk, Rst;
+    wire [31:0] WriteData, DataAddr;
+    wire MemWrite;
+
+    top a1(Clk, Rst, WriteData, DataAddr, MemWrite);
+
+    always begin
+        Clk <= 0;
+        #100;
+        Clk <= 1;
+        #100;
+    end
+
+    initial begin
+        Rst <= 1; #100;
+        Rst <= 0;
+    end
+
+endmodule;
+
